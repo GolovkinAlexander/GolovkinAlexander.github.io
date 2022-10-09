e06c67449bd28a9aa1b2eafcdef33a6eaf7eac80
Alexander Golovkin <55evg77@gmail.com>
2022-10-09 21:36:08 +0300

    jslint fix

--- script5.js
+++ script5.js
@@ -1,23 +1,22 @@
-window.addEventListener('DOMContentLoaded', function (event) 
+window.addEventListener("DOMContentLoaded", function (event)
 {
     console.log("DOM fully loaded and parsed");
-    
-    function calc(e) 
+    function calc(e)
     {
         e.preventDefault();
         let price = document.getElementById("price").value;
         let count = document.getElementById("count").value;
         let r = document.getElementById("result");
 
-        if(price == "" || count == "")
+        if(price === "" || count === "")
         {
-            alert("Все поля должны быть заполнены")
+            alert("Все поля должны быть заполнены");
         }
         else
-            if(isNaN(parseInt(price)) || isNaN(parseInt(count)))
-                alert("Вводить можно только числа")
+            if((parseInt(price).isNan) || (parseInt(count).isNan))
+            {alert("Вводить можно только числа");}
             else
-                r.innerHTML = parseInt(price)*parseInt(count) + " rub";
+            {r.innerHTML = parseInt(price)*parseInt(count) + " rub";}
     }
     let c = document.getElementById("my-button");
     c.addEventListener("click", calc);
