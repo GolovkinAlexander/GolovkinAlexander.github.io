6243b3349a793e7f43b733a75383f93d96c1c71c
Alexander Golovkin <55evg77@gmail.com>
2022-10-10 22:20:43 +0300

    add RegExp

--- script5.js
+++ script5.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function (event)
 {
     console.log("DOM fully loaded and parsed");
+
     function calc()
     {
         let price = document.getElementById("price").value;
@@ -8,26 +9,17 @@ window.addEventListener("DOMContentLoaded", function (event)
         let r = document.getElementById("result");
         
         
-        if(price.match(/^[0-9]+$/) != null)
+        let onlydigit=/^(?!(0))\d+$/;
+        if(price.match(onlydigit)===null || count.match(onlydigit)===null)
         {
-         console.log("String contains only numbers")
+          alert("Цена и количество должны быть положительными числами\n(Без незначащих 0)");
         }
         else
         {
-         console.log("String does not contain only numbers") 
+          r.innerHTML = parseFloat(price) * parseFloat(count);
         }
-        }
-        
-        if(price === "" || count === "")
-        {
-            alert("Все поля должны быть заполнены числами");
-        }
-        else
-            if (parseFloat(price)<0 || parseFloat(count) <0)
-            {alert("Цена и количество не могут быть отрицательными");}
-            else
-              {r.innerHTML = parseFloat(price)*parseFloat(count) + " rub";}
-    }
+      }
+
     let c = document.getElementById("my-button");
     c.addEventListener("click", calc);
     return false;
