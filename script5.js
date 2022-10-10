1f384448990e60ed880442f0e43654467395a2ba
GolovkinAlexander <112856471+GolovkinAlexander@users.noreply.github.com>
2022-10-10 11:01:34 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -12,7 +12,10 @@ window.addEventListener("DOMContentLoaded", function (event)
             alert("Все поля должны быть заполнены числами");
         }
         else
-            {r.innerHTML = parseFloat(price)*parseFloat(count) + " rub";}
+            if (price<0 || count <0)
+            {alert("Цена и количество не могут быть отрицательными";}
+            else      
+              {r.innerHTML = parseFloat(price)*parseFloat(count) + " rub";}
     }
     let c = document.getElementById("my-button");
     c.addEventListener("click", calc);
