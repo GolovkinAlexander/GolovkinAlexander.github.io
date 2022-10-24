42971d8e0eca357a78cec15f3193b6a357470aa6
GolovkinAlexander <112856471+GolovkinAlexander@users.noreply.github.com>
2022-10-24 17:48:14 +0300

    Update script7.js

--- script7.js
+++ script7.js
@@ -5,5 +5,30 @@ $(document).ready(function () {
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 4,
+			  slidesToScroll: 4,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2
+			}
+		  }
+		]
   });
 });
